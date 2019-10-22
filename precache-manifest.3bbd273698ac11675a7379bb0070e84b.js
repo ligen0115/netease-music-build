13872832d92bac56646408a21d6a58faf3a717b1
李根 <51691833+ligen0115@users.noreply.github.com>
2019-10-22 20:51:59 +0800

    上传

---
+++ precache-manifest.3bbd273698ac11675a7379bb0070e84b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40dc0ed4a8058815b4f48d92ca94a226",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb55ec7252a045785fc1",
+    "url": "./static/css/2.1b8912f3.chunk.css"
+  },
+  {
+    "revision": "e41adb7257b60ae30e00",
+    "url": "./static/css/main.25809260.chunk.css"
+  },
+  {
+    "revision": "cb55ec7252a045785fc1",
+    "url": "./static/js/2.3570f35d.chunk.js"
+  },
+  {
+    "revision": "e41adb7257b60ae30e00",
+    "url": "./static/js/main.a5419197.chunk.js"
+  },
+  {
+    "revision": "5685620d9ba7f4f9b562",
+    "url": "./static/js/runtime-main.5e2809da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a550590e6055976b78db9b8f104f2f4f.js"
+  "./precache-manifest.3bbd273698ac11675a7379bb0070e84b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
