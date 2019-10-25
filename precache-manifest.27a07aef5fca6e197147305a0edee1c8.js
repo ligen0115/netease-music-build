fed85858c73192d6e2bddbd1ce08ec35a6cf2479
李根 <51691833+ligen0115@users.noreply.github.com>
2019-10-25 22:37:15 +0800

    优化搜索页面

---
+++ precache-manifest.27a07aef5fca6e197147305a0edee1c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8918c5dc08ce2f5e9ef2f020a7ea42d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c3ea32698fb7ba90e2c",
+    "url": "./static/css/2.f3885999.chunk.css"
+  },
+  {
+    "revision": "32d7d47e273c0e43be98",
+    "url": "./static/css/main.c1c5abef.chunk.css"
+  },
+  {
+    "revision": "5c3ea32698fb7ba90e2c",
+    "url": "./static/js/2.40b2d6fc.chunk.js"
+  },
+  {
+    "revision": "32d7d47e273c0e43be98",
+    "url": "./static/js/main.b8d40eb7.chunk.js"
+  },
+  {
+    "revision": "5685620d9ba7f4f9b562",
+    "url": "./static/js/runtime-main.5e2809da.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.109a2535898542f50e30c33afcef5f53.js"
+  "./precache-manifest.27a07aef5fca6e197147305a0edee1c8.js"
 );
 
 self.addEventListener('message', (event) => {
