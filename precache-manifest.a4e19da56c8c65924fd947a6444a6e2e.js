742804e52f2d4139488c2279cf88bb1a62269986
李根 <51691833+ligen0115@users.noreply.github.com>
2019-10-26 16:22:05 +0800

    取消路由懒加载

---
+++ precache-manifest.a4e19da56c8c65924fd947a6444a6e2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc98df5a2ebec8af0548a3ff47a23fb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f40efb3a3fa20c77ded8",
+    "url": "./static/css/2.ca739fb7.chunk.css"
+  },
+  {
+    "revision": "0f144718394c16dd3e26",
+    "url": "./static/css/main.60b0e8ad.chunk.css"
+  },
+  {
+    "revision": "f40efb3a3fa20c77ded8",
+    "url": "./static/js/2.813a5817.chunk.js"
+  },
+  {
+    "revision": "0f144718394c16dd3e26",
+    "url": "./static/js/main.804e0560.chunk.js"
+  },
+  {
+    "revision": "5685620d9ba7f4f9b562",
+    "url": "./static/js/runtime-main.5e2809da.js"
+  }
+]);
